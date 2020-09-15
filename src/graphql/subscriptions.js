301e8bb5d69d32ccd93a33847410d1d1e24bf138
Takeuchi Dai <takeuchi.d@goodfellows.co.jp>
2020-09-15 14:37:43 +0900

    initファイルgit追加漏れ

---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,39 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateMessage = /* GraphQL */ `
+  subscription OnCreateMessage {
+    onCreateMessage {
+      id
+      name
+      title
+      description
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdateMessage = /* GraphQL */ `
+  subscription OnUpdateMessage {
+    onUpdateMessage {
+      id
+      name
+      title
+      description
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteMessage = /* GraphQL */ `
+  subscription OnDeleteMessage {
+    onDeleteMessage {
+      id
+      name
+      title
+      description
+      createdAt
+      updatedAt
+    }
+  }
+`;
